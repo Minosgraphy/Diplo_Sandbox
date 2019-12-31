29222b9e23136f345c30e7dda66c57c01e65cf3b
Minosgraphy <bjoern.m.waltert@gmail.com>
2019-12-31 10:55:54 +0100

    Reset for re-design

---
+++ Diplo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Diplo
+//
+//  Created by Björn Waltert on 31.12.19.
+//  Copyright © 2019 State Love. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
